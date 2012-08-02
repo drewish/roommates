4ec10af37393342af69f0010356c5358ea8d3ed7
andrew morton <drewish@katherinehouse.com>
2012-08-02 09:13:23 -0400

    Redid the Expense Summary.
    Simplified a lot of the parsing now that I understand it better. Also tried
    to make the current user's balance clearer.

--- roommates/ExpensesSummaryViewController.m
+++ roommates/ExpensesSummaryViewController.m
@@ -9,15 +9,28 @@
 #import "ExpensesSummaryViewController.h"
 #import "RMData.h"
 
-@interface ExpensesSummaryViewController ()
+@interface ExpensesSummaryItem : NSObject
+@property(retain) RMUser *user;
+@property(retain) NSDecimalNumber *amount;
++(id) itemWithUserId:(NSNumber*) userId forAmount:(NSDecimalNumber*) amount;
+@end
 
+@implementation ExpensesSummaryItem
+@synthesize user, amount;
++(id) itemWithUserId:(NSNumber*) userId forAmount:(NSDecimalNumber*) amount
+{
+    ExpensesSummaryItem *item = [self new];
+    item.user = [[RMUser users] objectForKey: userId];
+    item.amount = amount;
+    return item;
+}
 @end
 
 @implementation ExpensesSummaryViewController {
     PullToRefreshView *pull;
-    NSDictionary *balance;
-    NSDictionary *owed;
-    NSDictionary *owes;
+    NSDecimalNumber *myBalance;
+    NSMutableArray *top;
+    NSMutableArray *bottom;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -25,9 +38,9 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         // Custom initialization
-        balance = [NSDictionary dictionary];
-        owed = [NSDictionary dictionary];
-        owes = [NSDictionary dictionary];
+        myBalance = [NSDecimalNumber notANumber];
+        top = [NSMutableArray array];
+        bottom = [NSMutableArray array];
     }
     return self;
 }
@@ -68,9 +81,9 @@
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
     pull = nil;
-    balance = nil;
-    owed = nil;
-    owes = nil;
+    myBalance = nil;
+    top = nil;
+    bottom = nil;
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
@@ -82,26 +95,6 @@
 - (void)pullToRefreshViewShouldRefresh:(PullToRefreshView *)view;
 {
     [self fetchItems];
-}
-
-- (NSDictionary*)parseExpenseSection:(NSString*)keyPath inDictionary:(NSDictionary*)body
-{
-    NSMutableDictionary *ret = [NSMutableDictionary dictionary];
-    NSNumberFormatter * f = [NSNumberFormatter new];
-    f.numberStyle = NSNumberFormatterDecimalStyle;
-
-    if ([[body valueForKeyPath:keyPath] isKindOfClass:[NSDictionary class]]) {
-        NSMutableDictionary *d = [body valueForKeyPath:keyPath];
-        for (NSString *key in d) {
-            id val = [d objectForKey:key];
-            NSNumber *userId = [f numberFromString:key];
-            NSNumber *amount = ([val isKindOfClass:[NSNumber class]]) ? val : [f numberFromString:val];
-            if ([[RMUser users] objectForKey:userId]) {
-                [ret setObject:amount forKey:userId];
-            }
-        }
-    }
-    return [NSDictionary dictionaryWithDictionary:ret];
 }
 
 - (void)fetchItems {
@@ -116,9 +109,33 @@
         request.onDidLoadResponse = ^(RKResponse *response) {
             NSError *parseError = nil;
             NSDictionary *body = [response parsedBody:&parseError];
-            balance = [self parseExpenseSection:@"balances" inDictionary:body];
-            owed = [self parseExpenseSection:@"current.owed" inDictionary:body];
-            owes = [self parseExpenseSection:@"current.owes" inDictionary:body];
+
+            NSDecimalNumber *negativeOne = [NSDecimalNumber decimalNumberWithMantissa:1 exponent:0 isNegative:YES];
+            NSMutableDictionary *balances = [NSMutableDictionary dictionaryWithDictionary:
+                                             [body valueForKeyPath:@"balances"]];
+            NSNumber *myId = [RMSession instance].userId;
+            myBalance = [NSDecimalNumber decimalNumberWithString: [balances objectForKey:[myId stringValue]]];
+
+            // Put us at the top of both.
+            top = [NSMutableArray arrayWithObject:[ExpensesSummaryItem itemWithUserId:myId forAmount:myBalance]];
+            bottom = [NSMutableArray arrayWithObject:[ExpensesSummaryItem itemWithUserId:myId forAmount:myBalance]];
+
+            NSString *keyPath = (myBalance > 0) ? @"current.owed" : @"current.owes";
+            NSDictionary *totals = [body valueForKeyPath:keyPath];
+            for (NSString *key in totals) {
+                NSNumber *userId = @([key integerValue]);
+                NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:[totals objectForKey:key]];
+                [top addObject:[ExpensesSummaryItem itemWithUserId:userId forAmount:amount]];
+            }
+
+            for (NSString *key in balances) {
+                NSNumber *userId = @([key integerValue]);
+                // Skip the current user since we put ourselves at the top already.
+                if (![userId isEqualToNumber:myId]) {
+                    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:[balances objectForKey:key]];
+                    [bottom addObject:[ExpensesSummaryItem itemWithUserId:userId forAmount:amount]];
+                }
+            }
 
             [self.tableView reloadData];
             [pull finishedLoading];
@@ -153,20 +170,28 @@
 {
     switch (section) {
         case 0:
-            return owed.count + owes.count;
+            return top.count;
         case 1:
-            return balance.count;
+            return bottom.count;
     }
     return 0;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    switch (section) {
-        case 0:
-            return owed.count + owes.count ? @"Expense Summary" : nil;
-        case 1:
-            return balance.count ? @"Household Summary" : nil;
+    if (![myBalance isEqualToNumber:[NSDecimalNumber notANumber]]) {
+        if (section == 0) {
+            if ([myBalance floatValue] > 0) {
+                return @"You are owed";
+            }
+            else if ([myBalance floatValue] < 0) {
+                return @"You owe";
+            }
+            return @"You are paid up";
+        }
+        if (section == 1) {
+            return @"Household Summary";
+        }
     }
     return @"";
 }
@@ -175,54 +200,52 @@
 {
     // TODO: this whole thing is a giant hack that needs to be cleaned up.
     // I just want to get it showing the right stuff.
-    static NSString *cellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    NSArray *balanceUsers = [balance allKeys];
-    NSArray *owedUsers = [owed allKeys];
-    NSArray *owesUsers = [owes allKeys];
-    NSNumber *amount;
     NSString *action;
-    NSNumber *userId;
-    BOOL isSessionUser = FALSE;
     NSString *message = @"";
+    NSNumberFormatter * f = [NSNumberFormatter new];
+    f.numberStyle = NSNumberFormatterCurrencyStyle;
+    f.negativeFormat = f.positiveFormat;
     UIColor *ourRed = [UIColor colorWithRed:0.882 green:0.000 blue:0.000 alpha:1.000];
     UIColor *ourGreen = [UIColor colorWithRed:0.349 green:0.812 blue:0.125 alpha:1.000];
     UIColor *color = [UIColor colorWithRed:0.216 green:0.325 blue:0.525 alpha:1.000];
 
+    ExpensesSummaryItem *item;
+    NSString *cellIdentifier = @"Cell";
+    switch (indexPath.section) {
+        case 0:
+            item = [top objectAtIndex:indexPath.row];
+            cellIdentifier = (indexPath.row == 0) ? @"MyBalance" : @"Cell";
+            break;
+        case 1:
+            item = [bottom objectAtIndex:indexPath.row];
+            break;
+    }
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    NSDecimalNumber *amount = item.amount;
+
     // Configure the cell...
     switch (indexPath.section) {
         case 0:
-            if (indexPath.row < owedUsers.count) {
-                userId = [owedUsers objectAtIndex:indexPath.row];
-                action = @"owed";
-                color = ourGreen;
-                amount = [owed objectForKey:userId];
+            if (indexPath.row == 0) {
+                color = myBalance.floatValue < 0 ? ourRed : ourGreen;
+                cell.textLabel.text = [f stringFromNumber:amount];
+                cell.textLabel.textColor = color;
             }
             else {
-                userId = [owesUsers objectAtIndex:indexPath.row];
-                action = @"owes";
-                color = ourRed;
-                // Since we're flipping the color don't format negative number.
-                amount = [owes objectForKey:userId];
-            }
-
-            isSessionUser = [[RMSession instance].userId isEqualToNumber:userId];
-            if (isSessionUser) {
-                // TODO: Not sure this branch will fire. Might need to pull our
-                // total from the balances.
-                message = (action == @"owes") ? @"You owe:" : @"You are owed:";
-            }
-            else {
-                message = [NSString stringWithFormat:(action == @"owes") ? @"You owe %@:" : @"%@ owes you:", [RMUser nameForId: userId]];
+                if (myBalance.floatValue > 0) {
+                    action = @"owed";
+                    color = ourGreen;
+                }
+                else {
+                    action = @"owes";
+                    color = ourRed;
+                }
+                cell.textLabel.text = [NSString stringWithFormat:(action == @"owes") ? @"to %@:" : @"by %@", item.user.displayName];
             }
             break;
 
         case 1:
-            userId = [balanceUsers objectAtIndex:indexPath.row];
-            amount = [balance objectForKey:userId];
-            isSessionUser = [[RMSession instance].userId isEqualToNumber:userId];
-
-            if (isSessionUser) {
+            if (indexPath.row == 0) {
                 if ([amount floatValue] > 0) {
                     message = @"You are owed";
                     color = ourGreen;
@@ -234,6 +257,9 @@
                 else {
                     message = @"You are paid up";
                 }
+                cell.textLabel.text = message;
+                cell.textLabel.font = [UIFont boldSystemFontOfSize:17];
+                cell.detailTextLabel.font = [UIFont boldSystemFontOfSize:17];
             }
             else {
                 if ([amount floatValue] > 0) {
@@ -246,20 +272,11 @@
                 else {
                     message = @"%@ is paid up";
                 }
-                message = [NSString stringWithFormat:message, [RMUser nameForId: userId]];
+                cell.textLabel.text = [NSString stringWithFormat:message, item.user.displayName];
             }
             break;
     }
 
-    if (isSessionUser) {
-        cell.textLabel.font = [UIFont boldSystemFontOfSize:17];
-        cell.detailTextLabel.font = [UIFont boldSystemFontOfSize:17];
-    }
-    cell.textLabel.text = message;
-//    cell.textLabel.textColor = color;
-    NSNumberFormatter * f = [NSNumberFormatter new];
-    f.numberStyle = NSNumberFormatterCurrencyStyle;
-    f.negativeFormat = f.positiveFormat;
     cell.detailTextLabel.text = [f stringFromNumber:amount];
     cell.detailTextLabel.textColor = color;
 
